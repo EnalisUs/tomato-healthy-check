0d94822f46f4b04d1c5a7eb617c97fa3934cf49a
Enalis <86041093+EnalisUs@users.noreply.github.com>
2023-08-16 18:59:14 +0700

    Update 4_YOLO_for_video.py

--- pages/4_YOLO_for_video.py
+++ pages/4_YOLO_for_video.py
@@ -5,6 +5,7 @@ from yolo_predictions import YOLO_Pred  # Import your YOLO_Pred class
 import numpy as np
 import subprocess
 import base64
+import os
 with st.spinner('Please wait while your model is loading'):
     yolo_pred = YOLO_Pred(onnx_model='./models/v5.onnx',
                     data_yaml='./models/data.yaml')
